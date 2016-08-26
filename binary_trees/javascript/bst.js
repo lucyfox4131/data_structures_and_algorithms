ce830c3749fa09ec1d08cd244f3985e9d94379e3
Lucy Fox <lucyreneefox@gmail.com>
2016-08-26 14:31:42 -0600

    Add min/max and find

--- binary_trees/javascript/bst.js
+++ binary_trees/javascript/bst.js
@@ -39,3 +39,60 @@ function checkRight(data, currentNode){
     compareNodes(data, currentNode.right)
   }
 }
+
+BST.prototype.find = function (num) {
+  if (this.rootNode){
+    var node = findNode(num, this.rootNode)
+    return node
+  }else{
+    return null
+  }
+};
+
+function findNode(num, currentNode){
+  if(currentNode.data === num){
+    return currentNode
+  } else if (num <= currentNode.data){
+    return currentNode.left ? findNode(num, currentNode.left) : null
+  } else if (num > currentNode.data){
+    return currentNode.right ? findNode(num, currentNode.right) : null
+  }
+}
+
+BST.prototype.toArray = function () {
+  return []
+  // starting with the left side, return root, then left untill you get down to the bottom,
+  // then return all from the right side till you get to the bottom
+};
+
+BST.prototype.min = function () {
+  if(!this.rootNode){
+    return null
+  }else{
+    return findMin(this.rootNode)
+  }
+};
+
+function findMin(node){
+  if(node.left){
+    return findMin(node.left)
+  }else{
+    return node.data
+  }
+}
+
+BST.prototype.max = function () {
+  if(!this.rootNode){
+    return null
+  }else{
+    return findMax(this.rootNode)
+  }
+};
+
+function findMax(node){
+  if(node.right){
+    return findMax(node.right)
+  }else{
+    return node.data
+  }
+}
