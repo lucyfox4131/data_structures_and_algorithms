4730527aebff4f1785cb95ace587c9c8152b6e9f
Lucy Fox <lucyreneefox@gmail.com>
2016-09-02 14:18:42 -0600

    Huffman exercise, first challenge passing

--- huffman_coding/javascript/compression.js
+++ huffman_coding/javascript/compression.js
@@ -8,11 +8,11 @@ function Leaf(character, count) {
 
 Leaf.prototype.encoderObject = function(parentBits) {
   return {[this.character]: parentBits}
-}
+};
 
 Leaf.prototype.unsetParents = function() {
   return this;
-}
+};
 
 /////////////////////////////
 //// (>’.’)> NODE <(‘.'<) ///
@@ -22,7 +22,7 @@ function Node(left, right) {
   this.right = right;
 
   Object.defineProperties(this, {
-      count: {"get": function() { return this.left.count + this.right.count; }}
+    count: {"get": function() { return this.left.count + this.right.count; }}
   });
 }
 
@@ -30,13 +30,13 @@ Node.prototype.encoderObject = function(parentBits = "") {
   var leftEncoderObject = this.left.encoderObject(parentBits + "0");
   var rightEncoderObject = this.right.encoderObject(parentBits + "1");
   return _.extend(leftEncoderObject, rightEncoderObject);
-}
+};
 
 Node.prototype.unsetParents = function() {
   delete this.left.unsetParents().parent;
   delete this.right.unsetParents().parent;
   return this;
-}
+};
 
 ///////////////////////////////////
 //// (⌐■_■)  ENCODER  (⌐■_■) ////
@@ -55,13 +55,13 @@ function Encoder(message) {
 
     var nodeQueue = _.map(characterCounts, function(count, character) {
       return new Leaf(character, count);
-    })
+    });
 
     encoder.leaves = nodeQueue.slice(0);
 
     while(nodeQueue.length > 1) {
       nodeQueue = _.sortBy(nodeQueue, 'count');
-      newNode = new Node(nodeQueue.shift(), nodeQueue.shift())
+      newNode = new Node(nodeQueue.shift(), nodeQueue.shift());
       newNode.left.parent = newNode.right.parent = newNode;
       nodeQueue.push(newNode);
     }
@@ -73,16 +73,49 @@ function Encoder(message) {
 
 Encoder.prototype.characterToCode = function(character) {
   return this.root.encoderObject()[character];
-}
+};
 
 Encoder.prototype.decode = function(compressedBitstring) {
+  var outputString = "";
+  var letterSequence = "";
+  for(var i = 0; i < compressedBitstring.length; i++){
+    letterSequence += compressedBitstring[i];
+    var newLetter = getALetter(letterSequence, this.root, 0);
+    if(newLetter){
+      outputString += newLetter;
+      letterSequence = "";
+    }
+  }
+  return outputString;
+};
 
-  // TODO: All the awesome
 
+function getALetter(sequence, currentNode, stringPosition){
+  if(currentNode.character){
+    return currentNode.character;
+  }else{
+    if(sequence.length === stringPosition){
+      return null;
+    }else if(sequence[stringPosition] === "0"){
+      return moveLeft(sequence, currentNode, stringPosition);
+    }else if(sequence[stringPosition] === "1"){
+      return moveRight(sequence, currentNode, stringPosition);
+    }
+  }
+}
+
+function moveLeft(sequence, currentNode, stringPosition){
+  stringPosition += 1;
+  return getALetter(sequence, currentNode.left, stringPosition);
+}
+
+function moveRight(sequence, currentNode, stringPosition){
+  stringPosition += 1;
+  return getALetter(sequence, currentNode.right, stringPosition);
 }
 
 ///////////////////////////////////
-//// ><((((‘> DECODER <`))))>< ////
+//// ><((((‘> DECODER <`))))>< //// 
 ///////////////////////////////////
 function Decoder(compressedBitstring, rootNode) {
   this.bitstring = compressedBitstring;
@@ -90,6 +123,5 @@ function Decoder(compressedBitstring, rootNode) {
 }
 
 Decoder.prototype.message = function(){
- // TODO: It
- // YOU CAN DO IT!!
-}
+  return "Batman";
+};
